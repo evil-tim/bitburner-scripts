957634946ae246620162e63d2d3cbf474f0da875
James Villegas <evil-tim@users.noreply.github.com>
2022-08-18 14:01:37 +0800

    Update crime script

--- crime/s-main.js
+++ crime/s-main.js
@@ -1,56 +1,61 @@
 const SUCCESS_THRESHOLD = 0.8;
-const CRIMES = [
-	"traffick illegal arms",
-	"deal drugs",
-	"homicide",
-	"mug someone",
-	"shoplift"
-];
-
 const BOOTSTRAP_JOB = { company: "Joe's Guns", position: "Employee" };
+const CRIMES = [
+	{ code: "TRAFFICKARMS", name: "traffick illegal arms" },
+	{ code: "DRUGS", name: "deal drugs" },
+	{ code: "HOMICIDE", name: "homicide" },
+	{ code: "MUG", name: "mug someone" },
+	{ code: "SHOPLIFT", name: "shoplift" },
+];
 
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.disableLog("disableLog");
 	ns.disableLog("sleep");
 
-	ns.tail();
-
 	while (true) {
-		let didCrime = false;
-		for (const crime of CRIMES) {
-			const chance = ns.singularity.getCrimeChance(crime);
-			if (chance >= SUCCESS_THRESHOLD) {
-				didCrime = true;
-				ns.singularity.commitCrime(crime);
-				await spinLoop(ns, ns.singularity.isBusy);
-				ns.print("karma = " + await ns.heart.break());
-				ns.print("kills = " + ns.getPlayer().numPeopleKilled);
-				break;
-			}
+		const bestCrime = findBestCrime(ns);
+		const currWork = ns.singularity.getCurrentWork() ?? { type: "NONE" };
 
+		if (bestCrime === null) {
+			doCompanyJob(ns, currWork);
+		} else {
+			doCrime(ns, currWork, bestCrime);
 		}
 
-		if (!didCrime) {
-			const currentJobs = ns.getPlayer().jobs;
-			if (currentJobs[BOOTSTRAP_JOB.company] === undefined ||
-				currentJobs[BOOTSTRAP_JOB.company] !== BOOTSTRAP_JOB.position) {
-				ns.singularity.applyToCompany(BOOTSTRAP_JOB.company, BOOTSTRAP_JOB.position);
-			}
-			if (!ns.getPlayer().isWorking) {
-				ns.singularity.workForCompany(BOOTSTRAP_JOB.company, false);
-			}
-			await ns.sleep(1000);
-		}
+		ns.clearLog();
+		ns.print("karma = " + await ns.heart.break());
+		ns.print("kills = " + ns.getPlayer().numPeopleKilled);
+		await ns.sleep(2000);
 	}
 }
 
-/**
- * @param {NS} ns
- * @param {Function} condition
- */
-async function spinLoop(ns, condition) {
-	while (condition()) {
-		await ns.sleep(1000);
+/** @param {NS} ns */
+function doCompanyJob(ns, currWork) {
+	const currentJobs = ns.getPlayer().jobs;
+	if (currentJobs[BOOTSTRAP_JOB.company] === undefined ||
+		currentJobs[BOOTSTRAP_JOB.company] !== BOOTSTRAP_JOB.position) {
+		ns.singularity.applyToCompany(BOOTSTRAP_JOB.company, BOOTSTRAP_JOB.position);
+	}
+	if (currWork.type !== "COMPANY" && currWork.companyName !== BOOTSTRAP_JOB.company) {
+		ns.singularity.workForCompany(BOOTSTRAP_JOB.company, true);
+	}
+}
+
+/** @param {NS} ns */
+function findBestCrime(ns) {
+	for (const crime of CRIMES) {
+		const chance = ns.singularity.getCrimeChance(crime.name);
+		if (chance >= SUCCESS_THRESHOLD) {
+			return crime;
+		}
+	}
+	return null;
+}
+
+/** @param {NS} ns */
+function doCrime(ns, currWork, bestCrime) {
+	if (currWork.type !== "CRIME" || currWork.crimeType !== bestCrime.code) {
+		ns.singularity.commitCrime(bestCrime.name, false);
 	}
 }
